be05c739ef3024879d5ed0c46a0d3fd5a30b82e3
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-03-09 21:24:56 -0600

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
 app.get('*', (req, res) =>{
