f30630af16070b9315353c1d6f8d63ca67625036
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-03-16 20:01:47 -0500

    Added the edit functionality

--- models/places.js
+++ models/places.js
@@ -10,4 +10,10 @@ module.exports = [{
     state: 'AZ',
     cuisines: 'Pizza, Burgers',
     pic: '../images/ww.jpg'
+}, {
+    name: 'Magic Restroom Cafe',
+    city: 'City of Industry',
+    state:'CA',
+    cuisines: 'Taiwanese, Cafe',
+    pic:'../images/toiletseat.jpeg'
 }]
